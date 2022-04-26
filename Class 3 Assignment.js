de514aadeda550a2983a6257615e1e22033cf3b8
chandrulogan <78639242+chandrulogan@users.noreply.github.com>
2022-04-26 12:33:07 +0530

    ZEN CLASS 3 ASSIGNMENT

---
+++ Class 3 Assignment.js
@@ -0,0 +1,8 @@
+var resume={
+    first_name :"Chandru",
+    last_name  : "Loganathan",
+    Graduation : "B.E( Mechanical Engineering)",
+    Currentcourse : "FSD",
+    phone:"9789724829"
+};
+     console.log(resume);
